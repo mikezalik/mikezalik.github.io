a002c2edad511f8ab8fabe7bac9f3b178440c803
mikezalik <mike.zalik@gmail.com>
2019-02-21 18:24:20 -0800

    gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -6,3 +6,8 @@ concat = require('gulp-concat');
 gulp.task("hello", function() {
     console.log("hello!")
 });
+
+gulp.task('compileSass', function() {
+    gulp.src("input.scss")
+    .pipe(gulp.dest('styles.css'));
+})

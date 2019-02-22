d592edaea5a2234ccc7cbd95268b1b16421ec56b
mikezalik <mike.zalik@gmail.com>
2019-02-21 17:57:39 -0800

    gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -5,3 +5,7 @@ var gulp = require('gulp');
 gulp.task("hello", function() {
     console.log("hello!");
 });
+
+gulp.task("default", ["hello"], function() {
+    console.log("This is the default task!");
+});
